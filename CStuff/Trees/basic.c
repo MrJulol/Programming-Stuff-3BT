d2d6abf9273dc5e2b4f8c22aad46d11155307587
MrJulol <julian.thaler06@gmail.com>
2023-06-01 08:27:45 +0200

    hehe

--- CStuff/Trees/basic.c
+++ CStuff/Trees/basic.c
@@ -1,32 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node *root;
+struct node *root = NULL;
 
 struct node
 {
-    int number;
+    int data;
     struct node *right;
     struct node *left;
-    struct node *back;
 };
 
-void addbranch()
+void addNode()
 {
     int input;
     struct node *ptr = (struct node *)malloc(sizeof(struct node));
-    printf("Enter the next number");
-    scanf("%d", &input);
-    
+    if (ptr == NULL)
+    {
+        perror("Node creation");
+        exit(0);
+    }
+    else
+    {
+        printf("Enter a number: ");
+        scanf("%d", &input);
+        ptr->data = input;
+
+        if (root == NULL)
+        {
+            ptr->left = NULL;
+            ptr->right = NULL;
+            root = ptr;
+        }
+    }
+}
+
+int menu()
+{
+    int userInput = 0;
+    printf("1 = Add Node\n");
+    printf("2 = Print tree\n");
+    printf("3 = Delete Node\n");
+    printf("0 = EXIT");
+    scanf("%d", &userInput);
+    if (userInput != 1 || userInput != 2 || userInput != 3 || userInput != 0)
+    {
+        return menu();
+    }
+    else
+    {
+        return userInput;
+    }
 }
 
 main()
 {
-    root = (struct node *)malloc(sizeof(struct node));
-    printf("Enter first number");
-    scanf("%d", &root->number);
-
-    addbranch();
+    int user_Input = 1;
+    while (user_Input)
+    {
+        user_Input = menu();
+        switch (user_Input)
+        {
+        case 1:
+            addNode();
+            break;
+        case 2:
+            printTree();
+            break;
+        case 3:
+            delNode();
+            break;
+        default:
+            break;
+        }
+    }
 
     return EXIT_SUCCESS;
 }
